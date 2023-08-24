2edc39dd00e85777a6985cbebd8d798a2e8e817e
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-08-24 22:02:39 +0200

    stat commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,14 @@
-import { Blocks } from 'react-loader-spinner'
+import { Blocks } from 'react-loader-spinner';
 
 export const LoaderSpinner = () => {
-    return (
-        <Blocks 
-        visible={true}
-        height="55"
-        width="55"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        />
-    );
+  return (
+    <Blocks
+      visible={true}
+      height="50"
+      width="50"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{}}
+      wrapperClass="blocks-wrapper"
+    />
+  );
 };
