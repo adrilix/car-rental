f1f3fe8182cebe962b5d04fe1580be14d217907c
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-08-28 21:44:01 +0200

    statistic commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,12 +5,34 @@ function Modal(active, setActive, car) {
     img,
     make,
     model,
-    // ,year,rentalPrice,address,rentalCompany,description,type,id,accessories
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    description,
+    type,
+    id,
+    accessories,
   } = car;
+  const descrType = description => {
+    if (description.includes('lux') || description.includes('premium')) {
+      return 'Premium';
+    } else return '';
+  };
   return (
     <div>
       <svg />
       <img src={img} alt={`Car to rent ${make} ${model}`} />
+      <div>
+        <b>
+          {make}
+          <span>{model}</span>
+          {year}
+        </b>
+        <b>{rentalPrice}</b>
+      </div>
+      <div>{`${address}|${rentalCompany}|${descrType(description)}`}</div>
+      <div>{`${type}|${make}|${id}|${accessories[1]}`}</div>
       <button>Try call</button>
     </div>
   );
