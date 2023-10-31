bb1ddb6dff8532f1aa38e048bca14fdf2574efdf
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-10-31 23:59:40 +0100

    carsertige

--- src/Page/CatalogCars/CatalogPage.jsx
+++ src/Page/CatalogCars/CatalogPage.jsx
@@ -53,7 +53,7 @@ const CatalogPage = () => {
   };
 
   const handleRemoveFavorite = id => {
-    console.log('id: ', id);
+    console.log('id this car: ', id);
 
     dispatch(removeCarOnFavorite(id));
     console.log('remove on favorite', id);
