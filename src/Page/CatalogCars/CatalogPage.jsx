d5c41200063277134e44ff8b0fa07bb9e8ca1729
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-11-18 00:08:32 +0100

    pro memories fail

--- src/Page/CatalogCars/CatalogPage.jsx
+++ src/Page/CatalogCars/CatalogPage.jsx
@@ -53,7 +53,7 @@ const CatalogPage = () => {
   };
 
   const handleRemoveFavorite = id => {
-    console.log('id your car: ', id);
+    console.log('id car: ', id);
 
     dispatch(removeCarOnFavorite(id));
     console.log('remove on favorite', id);
