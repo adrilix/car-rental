b8428b935a869b41e414192fa8bb3a4262a54cef
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-12-31 16:13:40 +0100

    sokur fail

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,8 +4,8 @@ export const LoaderSpinner = () => {
   return (
     <Blocks
       visible={true}
-      height="60"
-      width="60"
+      height="40"
+      width="40"
       ariaLabel="blocks-loading"
       wrapperStyle={{}}
       wrapperClass="blocks-wrapper"
